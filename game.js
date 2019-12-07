f566aba71d2dc2a6070597208b9ae3583bc5d98a
your name here <your_email_here>
2019-12-07 17:24:19 -0500

    Test

--- game.js
+++ game.js
@@ -1,21 +1,23 @@
-// const readline = require('readline');
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-//   });
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
 class Towers{
     constructor(){
         this.rods = {A:[],B:[],C:[]}
         this.moveCount = 0
-
+        this.gameOver = false
     }
 
     setDiscs(){
         this.rods.A = [3,2,1]
     }
     isValidMove(oldRod,newRod){
-        if(!this.rods[newRod].length){
+        if(!this.rods[oldRod].length){
+            return false
+        } else if(!this.rods[newRod].length){
             return true
         } else if( this.rods[oldRod][(this.rods[oldRod]).length-1] > this.rods[newRod][(this.rods[newRod]).length-1]){
             return false
@@ -35,32 +37,63 @@ class Towers{
 
     }
     gameWon(){
-        if(this.rods["B"].length === 3 ||this.rods["C"] === 3){
+        if(this.rods["B"].length === 3 ||this.rods["C"].length === 3){
             return true
         } else {
             return false
         }
     }
+
     getMove(){
+        console.log(this.rods)
+        this.gameWon()
+        if(!this.gameWon()) {
+             return this.results()
+        } else {
+            
+            rl.question(`${this.rods}What rod would you like to move?`, (oldRod)=>{
+                rl.question("To which new rod would you like to move it to?", (newRod)=>{
+                    this.move(oldRod,newRod);
+                    console.log(this.rods)
+                // rl.close();
+                }
         
+            )}
+            )
+        }
+         this.getMove()
     }
-    
+
+    results(){
+        console.log("end")
+        rl.close()
+    }
+
+    // play(){
+    //     (!this.gameOver){
+    //         this.getMove()
+    //     }
+    // }
 }
 
 let towers = new Towers()
 
+// towers.play()
 towers.setDiscs()
+towers.getMove()
 
-console.log(towers.rods)
-towers.move("A","C")
-towers.move("A","B")
-console.log(towers.rods)
-towers.move("B","C")
-towers.move("C","A")
-towers.move("A","B")
-towers.move("A","B")
-towers.move("B","C")
-towers.move("B","A")
+// console.log(towers.rods)
+// towers.move("A","C")
+// towers.move("A","B")
+// console.log(towers.rods)
+// towers.move("B","C")
+// towers.move("C","A")
+// towers.move("A","B")
+// towers.move("A","B")
+// towers.move("B","C")
+// towers.move("B","A")
 
-console.log(towers.rods)
-console.log(towers.moveCount)
+// console.log(towers.rods)
+// console.log(towers.moveCount)
+
+
