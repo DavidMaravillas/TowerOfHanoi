d0d42020dac11e38bd18d322f623a71ac7f8a652
david maravillas <davidmaravillas@pursuit.org>
2019-12-07 16:06:41 -0500

    disk move

--- game.js
+++ game.js
@@ -6,6 +6,28 @@ const rl = readline.createInterface({
 
 class Towers{
     constructor(){
-        this.towers =towers
+        this.rods = {A:[],B:[],C:[]}
+
     }
+
+    setDiscs(){
+        this.rods.A = [3,2,1]
+    }
+    move(oldRod,newRod){
+
+        this.rods[newRod].push(this.rods[oldRod][(this.rods[oldRod]).length-1])
+        this.rods[oldRod].pop()
+    }
+
+    
 }
+
+let towers = new Towers()
+
+towers.setDiscs()
+
+console.log(towers.rods)
+towers.move("A","C")
+towers.move("A","B")
+console.log(towers.rods)
+
